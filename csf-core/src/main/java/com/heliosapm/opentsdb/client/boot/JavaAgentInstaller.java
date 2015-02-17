f369486306db7fb48bf4bd40453c04ab4faf186c
Nicholas Whitehead <nwhitehead@heliosdev.org>
2015-02-17 09:52:40 -0500

    Added list and listjson command line options to the AgentInstaller

--- csf-core/src/main/java/com/heliosapm/opentsdb/client/boot/JavaAgentInstaller.java
+++ csf-core/src/main/java/com/heliosapm/opentsdb/client/boot/JavaAgentInstaller.java
@@ -17,9 +17,13 @@ package com.heliosapm.opentsdb.client.boot;
 
 import java.io.File;
 import java.lang.management.ManagementFactory;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.heliosapm.attachme.VirtualMachine;
 import com.heliosapm.attachme.VirtualMachineDescriptor;
@@ -34,6 +38,11 @@ import com.heliosapm.attachme.VirtualMachineDescriptor;
 
 public class JavaAgentInstaller {
 
+	/** This JVMs PID as a long */
+	public static final long myPid = Long.parseLong(ManagementFactory.getRuntimeMXBean().getName().split("@")[0]);
+	/** This JVMs PID as a String */
+	public static final String myId = Long.toString(myPid);
+	
 	/**
 	 * Creates a new JavaAgentInstaller
 	 */
@@ -64,6 +73,12 @@ public class JavaAgentInstaller {
 		final long pid;
 		if(isPid(args[0])) {
 			pid = Long.parseLong(args[0].trim());
+		} else if("-list".equalsIgnoreCase(args[0])) {
+			printJVMs(myId);
+			return;
+		} else if("-listjson".equalsIgnoreCase(args[0])) {
+			printJVMsInJSON(myId);
+			return;			
 		} else {
 			pid = findPid(args[0]);
 		}
@@ -101,28 +116,92 @@ public class JavaAgentInstaller {
 		}		
 	}
 	
-	// INSTALL SYSPROP !!
+	/**
+	 * Prints the running JVMs in plain text
+	 * @param myId The id of this JVM so's we don't print it
+	 */
+	public static void printJVMs(final String myId) {
+		for(VirtualMachineDescriptor vmd: VirtualMachine.list()) {
+			if(myId.equals(vmd.id())) continue;
+			String vmDisplay = vmd.displayName();
+			if(vmDisplay==null || vmDisplay.trim().isEmpty()) {
+				vmDisplay = "Unknown";
+			}			
+			log("\n\tPID: [%s], Display: [%s]", vmd.id(), vmDisplay);
+		}		
+	}
+	
+	/**
+	 * Prints the running JVMs in plain text
+	 * @param myId The id of this JVM so's we don't print it
+	 */
+	public static void printJVMsInJSON(final String myId) {
+		final JSONArray arr = new JSONArray();
+		for(VirtualMachineDescriptor vmd: VirtualMachine.list()) {
+			if(myId.equals(vmd.id())) continue;
+			String vmDisplay = vmd.displayName();
+			if(vmDisplay==null || vmDisplay.trim().isEmpty()) {
+				vmDisplay = "Unknown";
+			}			
+			JSONObject vm = new JSONObject();
+			vm.put("pid", Long.parseLong(vmd.id().trim()));
+			vm.put("display", vmDisplay); 
+			arr.put(vm);
+		}
+		log(arr.toString(2));
+	}
+	
+	public static String formatVMDisplay(final VirtualMachineDescriptor vmd) {
+		String commandLine = vmd.displayName();
+        int firstSpace = commandLine.indexOf(' ');
+        if (firstSpace > 0) {
+            commandLine = commandLine.substring(firstSpace + 1);
+        }
+        String arg0 = commandLine;
+        int lastFileSeparator = arg0.lastIndexOf('/');
+        if (lastFileSeparator > 0) {
+             return arg0.substring(lastFileSeparator + 1);
+        }
+
+        lastFileSeparator = arg0.lastIndexOf('\\');
+        if (lastFileSeparator > 0) {
+             return arg0.substring(lastFileSeparator + 1);
+        }
+
+        int lastPackageSeparator = arg0.lastIndexOf('.');
+        if (lastPackageSeparator > 0) {
+             return arg0.substring(lastPackageSeparator + 1);
+        }        
+        return arg0;
+	}
 	
 	private static long findPid(final String argZero) {
 		Pattern p = Pattern.compile(argZero.trim());
 		final Map<Long, String> matchedPids = new HashMap<Long, String>();
 		final Map<Long, String> allPids = new HashMap<Long, String>();
-		final long myPid = Long.parseLong(ManagementFactory.getRuntimeMXBean().getName().split("@")[0]);
+		
 		for(VirtualMachineDescriptor vmd: VirtualMachine.list()) {
 			long pid = Long.parseLong(vmd.id().trim());
 			if(pid==myPid) continue;
-			allPids.put(pid, vmd.displayName());
-			if(!p.matcher(vmd.displayName()).matches()) continue;
+			String vmDisplay = 
+					//formatVMDisplay(vmd);   
+					vmd.displayName();
+			if(vmDisplay==null || vmDisplay.trim().isEmpty()) {
+				vmDisplay = "Unknown";
+			}
+					
+			allPids.put(pid, vmDisplay);
+			if(!p.matcher(vmDisplay).matches()) continue;
 			matchedPids.put(pid, vmd.displayName());
 		}
 		if(matchedPids.isEmpty()) {
-			log("Failed to find any JVM processes matching [%s]\nAvailable JVMs are:", argZero);
-			for(Map.Entry<Long, String> entry: matchedPids.entrySet()) {
+			log("\n\t=======================================================\n\tFailed to find any JVM processes matching [%s]. Available JVMs follow:\n\t=======================================================", argZero);
+			for(Map.Entry<Long, String> entry: allPids.entrySet()) {
 				log("\n\tPID: [%s], Display: [%s]", entry.getKey(), entry.getValue());
 			}			
 			return -1L;
 		} else if(matchedPids.size() != 1) {
-			log("Matched more than one JVM process. Processes found were:");
+			log("\n\t=======================================================\n\tMatched more than one JVM process. Processes found follow:\n\t=======================================================");
 			for(Map.Entry<Long, String> entry: matchedPids.entrySet()) {
 				log("\n\tPID: [%s], Display: [%s]", entry.getKey(), entry.getValue());
 			}
